dc85ed3262f92090bfceb5a206ed9ac6749b7a21
Alexandr <ak4raulov@yandex.ru>
2021-12-12 16:40:45 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const text = 'Hello World!'
+const elem = (
+	<div>
+		<h2 className ='red'>Текс из переменной {text}</h2>
+		{/*<h2>{ 2 * 2}</h2>*/}
+		<input type='text' />
+		<button tabIndex='0'>click</button>
+	</div>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	elem,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
