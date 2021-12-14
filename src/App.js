f74e4a755dc92866313ac31e6da13b8c608f814b
Alexandr <ak4raulov@yandex.ru>
2021-12-14 14:10:39 +0300

    Знакомство с реакт, элементы, компоненты, строгий режим

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
-import logo from './logo.svg';
+import {Component, StrictMode} from 'react';
 import './App.css';
+
+const Header = () => {
+	return <h2>Hello world!</h2>
+}
+
+//const Field = () => {
+//	const holder = 'Enter here'
+//	const styledField = {
+//		width: '300px',
+//		color: 'red'
+//	}
+//	return <input
+//		placeholder={holder}
+//		type="text"
+//		style={styledField} />
+//}
+
+class Field extends Component{
+	render() {
+		const holder = 'Enter here'
+		const styledField = {
+			width: '300px',
+			color: 'red'
+		}
+		return <input
+			placeholder={holder}
+			type="text"
+			style={styledField} />
+	}
+}
+
+function Btn() {
+	const text = 'Log in'
+	const logged = false;
+	return <button>{logged ? 'Enter': text}</button> 
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-      </header>
+		  <StrictMode>
+			  <Header />
+		  </StrictMode>
+		  <Field />
+		  <Btn />
     </div>
   );
 }
 
+export {Header};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-const text = 'Hello World!'
-const elem = (
-	<div>
-		<h2 className ='red'>Текс из переменной {text}</h2>
-		{/*<h2>{ 2 * 2}</h2>*/}
-		<input type='text' />
-		<button tabIndex='0'>click</button>
-	</div>
-)
+import {Header} from './App'
 
 ReactDOM.render(
-	elem,
+	<StrictMode>
+		<App/>
+	</StrictMode>,
   document.getElementById('root')
 );
